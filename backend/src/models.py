b5915480f4d5e845bea8ac4dd291ba0ff2dfdf16
Danila <danilazmiter@gmail.com>
2026-07-23 17:26:32 +0300

    feat(ORM): add method that give name to all children orm models of Base

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,9 +1,13 @@
 from datetime import datetime
 from sqlalchemy import func
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
 
 
 class BaseORM(DeclarativeBase):
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return cls.__name__[:-3].lower()
+
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     updated__at: Mapped[datetime] = mapped_column(
         server_default=func.now(), onupdate=func.now()
