2adb20cc03affab9cafb9948845d2f897f5a260c
Danila <danilazmiter@gmail.com>
2026-07-23 16:13:14 +0300

    feat(ORM): create basic ORM model with time fields

---
+++ backend/src/models.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from sqlalchemy import func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class BaseORM(DeclarativeBase):
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated__at: Mapped[datetime] = mapped_column(
+        server_default=func.now(), onupdate=func.now()
+    )
